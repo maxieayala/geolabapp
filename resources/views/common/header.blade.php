e1c092784dc5772b16411e0613f7de61715f909d
MaxieAyala <MAyala@credifacilnicaragua.com>
2023-04-06 20:34:50 -0600

    Cambos en menu y archivos de migracion 	new file:   database/migrations/2023_04_06_175326_create_estudio_table.php 	new file:   database/migrations/2023_04_06_175851_create_muestra_table.php 	modified:   resources/views/common/header.blade.php modified:   resources/views/common/sidebar.blade.php

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -25,7 +25,9 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                    {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}
+                </span>
                 <img class="img-profile rounded-circle" src="{{ asset('admin/img/undraw_profile.svg') }}">
             </a>
             <!-- Dropdown - User Information -->
--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -1,22 +1,55 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-university"></i>
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-cubes"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Tech-Admin</div>
+        <div class="sidebar-brand-text mx-3">Geolapp</div>
     </a>
+    <!-- Nav Item - Pages Collapse Menu -->
+    
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Laboratorio
+    </div>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#taTpDropDown"
+            aria-expanded="true" aria-controls="taTpDropDown">
+            <i class="fas fa-user-alt"></i>
+            <span>Muestras de Laboratorio</span>
+        </a>
+        <div id="taTpDropDown" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Muestras</h6>
+                <a class="collapse-item" href="{{ route('users.index') }}">Muestras</a>
+                <a class="collapse-item" href="{{ route('users.create') }}">Imagenes de campo</a>
+
+            </div>
+        </div>
+    </li>
+    <!-- Divider -->
+
+
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#taTpDropDown"
+            aria-expanded="true" aria-controls="taTpDropDown">
+            <i class="fas fa-user-alt"></i>
+            <span>Proyectos</span>
+        </a>
+        <div id="taTpDropDown" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Administracion</h6>
+                <a class="collapse-item" href="{{ route('users.index') }}">Proyectos</a>
+                <a class="collapse-item" href="{{ route('users.create') }}">Clientes</a>
+
+            </div>
+        </div>
+    </li>
 
     <!-- Divider -->
-    <hr class="sidebar-divider my-0">
-
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('home') }}">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
