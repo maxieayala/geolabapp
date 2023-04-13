a8d040ab9b780812175fcec62713edbeb2da87db
mijaberes1 <mijaberes1@gmail.com>
2023-04-12 21:56:22 -0600

    mas labels

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -19,7 +19,7 @@
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Reset Password!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">Reestablecer contraseña</h1>
                                 </div>
 
                                 @if (session('error'))
@@ -64,7 +64,7 @@
                                     </div>
 
                                     <button class="btn btn-primary btn-user btn-block" type="submit">
-                                        {{ __('Reset Password') }}
+                                        {{ __('Reestablecer contraseña') }}
                                     </button>
                                 </form>
                             </div>
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verificar el correo electronico') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Un nuevo enlace de confirmacion ha sido enviado a tu correo electronico.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Antes de proceder, por favor revisa tu correo y busca el enlace de confirmacion.') }}
+                    {{ __('Si no recibiste el correo') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('presiona aca para solicitar otro') }}</button>.
                     </form>
                 </div>
             </div>
