d33062dadbde8b41c4727b56662c4bc2a3d02388
mijaberes1 <mijaberes1@gmail.com>
2023-04-12 21:29:20 -0600

    Update login.blade.php
    actualice los signos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Bienvenido!</h1>
+                                    <h1 class="h4 text-gray-900 mb-4">¡Bienvenido!</h1>
                                 </div>
 
                                 @if (session('error'))
@@ -66,7 +66,7 @@
                                 </form>
                                 <hr>
                                 <div class="text-center">
-                                    <a class="small" href="{{ route('password.request') }}">¿Olvido su contraseña?</a>
+                                    <a class="small" href="{{ route('password.request') }}">¿Olvidó su contraseña?</a>
                                 </div>
                             </div>
                         </div>
