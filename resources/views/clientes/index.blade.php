a96f6064d74176775137763c4c8cd179b0f4ff64
MaxieAyala <MAyala@credifacilnicaragua.com>
2023-04-12 22:19:33 -0600

    tag error

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -22,89 +22,87 @@
 
         </div>
     </div>
-   <!-- DataTales Example -->
-   <div class="card shadow mb-4">
-
-    {{-- Alert Messages --}}
-    @include('common.alert')
-
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Clientes</h6>
-            <form method="GET" action="{{ route('clientes') }}"
-            class="d-sm-inline-block form-inline navbar-search w-50">
-            <div class="py-2 flex">
-                <div class="input-group mb-3">
-            
-                    <input type="search" name="search" id="search" value="{{ request()->input('search') }}"
-                        class="form-control small" placeholder="Nombre cliente"
-                        aria-label="Search" aria-describedby="basic-addon2">
 
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-primary" type='submit'>
-                            <i class="fas fa-search fa-sm"></i>
-                            Buscar
-                        </button>
-    
+        {{-- Alert Messages --}}
+        @include('common.alert')
 
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Clientes</h6>
+                <form method="GET" action="{{ route('clientes') }}"
+                    class="d-sm-inline-block form-inline navbar-search w-50">
+                    <div class="py-2 flex">
+                        <div class="input-group mb-3">
+
+                            <input type="search" name="search" id="search" value="{{ request()->input('search') }}"
+                                class="form-control small" placeholder="Nombre cliente" aria-label="Search"
+                                aria-describedby="basic-addon2">
+
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-primary" type='submit'>
+                                    <i class="fas fa-search fa-sm"></i>
+                                    Buscar
+                                </button>
+
+
+                            </div>
+
+
+                        </div>
                     </div>
 
+                </form>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+
+                                <th width="5%">Nombre cliente</th>
+                                <th width="5%">email</th>
+                                <th width="5%">telefono</th>
+                                <th width="5%">direccion</th>   
+                                <th width="5%">Estatus</th>
+                                <th width="3%">Opciones</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($clientes as $cliente)
+                            <tr>
+                                <td>{{$cliente->nombre}}</td>
+                                <td>{{$cliente->email}}</td>
+                                <td>{{$cliente->telefono}}</td>
+                                <td>{{$cliente->direccion}}</td>
+                                <td style="display: flex">
+                                    <a href="{{ route('clientes.edit', ['cliente' => $cliente->id]) }}"
+                                        class="btn btn-primary m-2">
+                                        <i class="fa fa-pen"></i>
+                                    </a>
+                                    <a class="btn btn-danger m-2" href="#" data-toggle="modal"
+                                        data-target="#deleteModal">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                            </tr>
+                            @empty
+                            <tr>
+                                <span style="color:red;">*</span>SIN REGISTROS
+                            </tr>
+                            @endforelse
+
+                        </tbody>
+                    </table>
+                    {{ $clientes->links() }}
 
                 </div>
             </div>
-
-        </form>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
 
-
-
-
-                            <th width="5%">Nombre cliente</th>
-
-                            <th width="5%">Estatus</th>
-
-
-                            <th width="3%">Opciones</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($clientes as $cliente)
-                        <tr>
-                            <td>{{$cliente->nombre}}</td>
-                            <td>{{$cliente->email}}</td>
-                            <td>{{$cliente->telefono}}</td>
-                            <td>{{$cliente->direccion}}</td>
-                            <td style="display: flex">
-                                <a href="{{ route('clientes.edit', ['cliente' => $cliente->id]) }}"
-                                    class="btn btn-primary m-2">
-                                    <i class="fa fa-pen"></i>
-                                </a>
-                                <a class="btn btn-danger m-2" href="#" data-toggle="modal" data-target="#deleteModal">
-                                    <i class="fas fa-trash"></i>
-                                </a>
-                        </tr>
-                        @empty
-                        <tr>
-                            <span style="color:red;">*</span>SIN REGISTROS
-                        </tr>
-                        @endforelse
-
-                    </tbody>
-                </table>
-                {{ $clientes->links() }}
-
-            </div>
-        </div>
     </div>
 
-</div>
 
-
-@endsection
+    @endsection
